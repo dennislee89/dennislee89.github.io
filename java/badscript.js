1c1f5c6109988d05132204fa5595f08df11e61a8
dennislee89 <dennislee1989@gmail.com>
2017-10-18 21:19:22 -0500

    added smooth scroll with jquery

--- java/badscript.js
+++ java/badscript.js
@@ -59,3 +59,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";   
 }
+
+
+
+$('li').click(function(e) {
+
+  // Create a variable with the link found in the list-item
+  var link = $(this).children('a');      
+  
+  $('html,body').animate({
+    scrollTop: $(link.attr('href')).offset().top
+  }, 2000);
+});
